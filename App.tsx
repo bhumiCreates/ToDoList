c625293b896b59b0062d738f734a7151c71583ef
bhumiCreates <bhumiwadhwani.work@gmail.com>
2026-05-13 13:39:21 +0530

    Final commit

--- App.tsx
+++ App.tsx
@@ -1,36 +1,112 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+// import { NewAppScreen } from '@react-native/new-app-screen';
+// import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+// import {
+//   SafeAreaProvider,
+//   useSafeAreaInsets,
+// } from 'react-native-safe-area-context';
+
+// function App() {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   return (
+//     <SafeAreaProvider>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <AppContent />
+//     </SafeAreaProvider>
+//   );
+// }
+
+// function AppContent() {
+//   const safeAreaInsets = useSafeAreaInsets();
+
+//   return (
+//     <View style={styles.container}>
+//       <NewAppScreen
+//         templateFileName="App.tsx"
+//         safeAreaInsets={safeAreaInsets}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+
+// export default App;
+
+import React, { useState } from "react";
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  View,
+  Text,
+  TextInput,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+type Todo = {
+  id: string;
+  text: string;
+};
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+export default function App() {
+  const [task, setTask] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+  const addTodo = () => {
+    if (task.trim() === "") return;
+
+    const newTodo: Todo = {
+      id: Date.now().toString(),
+      text: task,
+    };
+
+    setTodos((prev) => [...prev, newTodo]);
+    setTask("");
+  };
+
+  const deleteTodo = (id: string) => {
+    setTodos((prev) => prev.filter((item) => item.id !== id));
+  };
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+      <Text style={styles.title}>To-Do List </Text>
+
+      <TextInput
+        placeholder="Enter task..."
+        value={task}
+        onChangeText={setTask}
+        style={styles.input}
+      />
+
+      <Button title="Add Task" onPress={addTodo} />
+
+      <FlatList
+        data={todos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => deleteTodo(item.id)}
+            style={styles.todo}
+          >
+            <Text style={styles.todoText}>{item.text}</Text>
+            <Text style={styles.delete}>Done</Text>
+            
+            
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
@@ -39,7 +115,40 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
+    paddingTop: 60,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 8,
+  },
+  todo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 12,
+    marginTop: 10,
+    backgroundColor: "#f2f2f2",
+    borderRadius: 8,
+  },
+  todoText: {
+    fontSize: 16,
+  },
+  delete: {
+    paddingTop:2.2,
+    textAlign:"center",
+    height:28,
+    width:70,
+    backgroundColor: "#40ef00",
+    borderRadius: 15,
+    fontSize: 16,
   },
 });
-
-export default App;
